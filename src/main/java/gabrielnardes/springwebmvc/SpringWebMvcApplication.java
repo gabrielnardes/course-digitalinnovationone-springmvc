3579ff244b470ed1bc66011c2e9089b359f45066
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-06-09 15:25:36 -0300

    Add JediController

--- src/main/java/gabrielnardes/springwebmvc/SpringWebMvcApplication.java
+++ src/main/java/gabrielnardes/springwebmvc/SpringWebMvcApplication.java
@@ -8,6 +8,6 @@ public class SpringWebMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebMvcApplication.class, args);
+		System.out.println("Server on");
 	}
-
 }
---
+++ src/main/java/gabrielnardes/springwebmvc/controller/JediController.java
@@ -0,0 +1,13 @@
+package gabrielnardes.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JediController {
+
+    @GetMapping("/jedi")
+    public String jedi() {
+        return "jedi";
+    }
+}
