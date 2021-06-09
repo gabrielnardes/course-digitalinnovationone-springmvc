d8b20f55825ac5bb2c9a13c67ded1b2ce2b222b0
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-06-09 15:31:50 -0300

    Add ModelAndView return

--- src/main/java/gabrielnardes/springwebmvc/controller/JediController.java
+++ src/main/java/gabrielnardes/springwebmvc/controller/JediController.java
@@ -1,13 +1,20 @@
 package gabrielnardes.springwebmvc.controller;
 
+import gabrielnardes.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JediController {
 
     @GetMapping("/jedi")
-    public String jedi() {
-        return "jedi";
+    public ModelAndView jedi() {
+        final ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("jedi");
+        modelAndView.addObject("allJedi", new Jedi("Luke", "Skywalker"));
+
+        return modelAndView;
     }
 }
