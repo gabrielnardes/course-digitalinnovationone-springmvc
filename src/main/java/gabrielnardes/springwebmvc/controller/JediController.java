025519c3e52923d9dcdbd33de78a93df0f0e6620
Gabriel Nardes Giampietro <gabrielnardeslobao@gmail.com>
2021-06-13 14:46:41 -0300

    Add Not Black validation

--- src/main/java/gabrielnardes/springwebmvc/controller/JediController.java
+++ src/main/java/gabrielnardes/springwebmvc/controller/JediController.java
@@ -4,10 +4,13 @@ import gabrielnardes.springwebmvc.model.Jedi;
 import gabrielnardes.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class JediController {
@@ -36,7 +39,11 @@ public class JediController {
     }
 
     @PostMapping("/jedi")
-    public String createJedi(@ModelAttribute Jedi jedi) {
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result) {
+        if (result.hasErrors()) {
+            return "new-jedi";
+        }
+
         repository.add(jedi);
 
         return "redirect:jedi";
