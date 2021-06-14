0fed5d4e15df300073e7a7e9e9bacda4af7fb108
Gabriel Nardes Giampietro <gabrielnardeslobao@gmail.com>
2021-06-14 14:36:53 -0300

    Add status code to GET

--- spring-web-mvc-data-basic/src/main/java/gabrielnardes/springwebmvc/rest/JediResource.java
+++ spring-web-mvc-data-basic/src/main/java/gabrielnardes/springwebmvc/rest/JediResource.java
@@ -1,9 +1,9 @@
 package gabrielnardes.springwebmvc.rest;
 
-import gabrielnardes.springwebmvc.exception.JediNotFoundException;
 import gabrielnardes.springwebmvc.model.Jedi;
 import gabrielnardes.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +22,13 @@ public class JediResource {
     }
 
     @GetMapping("/api/jedi/{id}")
-    public Jedi getJedi(@PathVariable Long id) {
+    public ResponseEntity<Jedi> getJedi(@PathVariable Long id) {
         final Optional<Jedi> jedi =  repository.findById(id);
 
         if (jedi.isPresent()) {
-            return jedi.get();
+            return ResponseEntity.ok(jedi.get());
         } else {
-            throw new JediNotFoundException();
+            return ResponseEntity.notFound().build();
         }
     }
 }
