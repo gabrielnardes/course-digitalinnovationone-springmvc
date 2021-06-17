832681ccd73c4587400095b9644d9e1dc20aa9ad
Gabriel Nardes Giampietro <gabrielnardeslobao@gmail.com>
2021-06-16 22:20:43 -0300

    Add soldado repository

---
+++ jax-rs/src/main/java/gabrielnardes/jaxrs/repository/SoldadoRepository.java
@@ -0,0 +1,9 @@
+package gabrielnardes.jaxrs.repository;
+
+import gabrielnardes.jaxrs.entity.SoldadoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoldadoRepository extends JpaRepository<SoldadoEntity, Long> {
+}
